879a31098ef21bee4e15c4b5c55de21086b77ffc
acronymcreations@gmail.com <acronymcreations@gmail.com>
2016-11-30 09:48:20 -0700

    minor changes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from google.appengine.ext import db
 import re
 import logging
 import hashlib
+import time
 
 template_dir = os.path.join(os.path.dirname(__file__),'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape=True)
@@ -39,7 +40,6 @@ class Handler(webapp2.RequestHandler):
         return hash_text
 
     def verify_cookie(self,val):
-        logging.info(val)
         val_a = val.split('|')
         if self.hash_str(val_a[0]) == val_a[1]:
             return val_a[0]
@@ -51,18 +51,19 @@ class Handler(webapp2.RequestHandler):
             if verification:
                 logging.info("cookie verified")
                 u = User.get_by_id(int(verification),parent = None)
-                logging.info(u.username)
                 return u
 
     def set_secure_cookie(self,user_id):
         hash_id = self.hash_str(user_id)
-        self.response.headers.add_header('Set-Cookie','%s|%s'%(user_id,hash_id))
+        self.response.headers.add_header('Set-Cookie','user_id=%s|%s;PATH=/'%(user_id,hash_id))
 
 class MainHandler(Handler):
     def get(self):
-    	enteries = db.GqlQuery('select * from codeobject order by created desc')
-        self.render('main.html',enteries = enteries)
-        mgw = self.request.cookies.get('MGW','false')
+        u = self.check_id_cookie()
+    	enteries = db.GqlQuery('select * from PostObject order by created desc')
+        logging.info(enteries[0].likes)
+        logging.info(len(enteries[0].likes))
+        self.render('main.html',enteries = enteries, user = u)
 
 class Signup(Handler):
     def get(self):
@@ -83,6 +84,10 @@ class Signup(Handler):
         params = {'username':username}
         valid_form = True
 
+        already_exists = db.GqlQuery("select * from User where username = '%s'"%username).get()
+        if already_exists:
+            params['error_username'] = 'Username is already taken'
+            valid_form = False
         if not valid_username(username):
             params['error_username'] = 'Username is too short'
             valid_form = False
@@ -99,16 +104,15 @@ class Signup(Handler):
 
         if valid_form:
             hash_password = self.hash_str(password)
-            new_user = User(username = username, password = hash_password, email = email)
+            new_user = User(username = username, password = hash_password, email = email,likes = [])
             new_user_key = new_user.put()
             user_id = new_user_key.id()
             id_hash = self.hash_str(user_id)
-            self.response.headers.add_header('Set-Cookie',str('user_id=%s|%s; PATH=/'%(user_id,id_hash)))
+            self.set_secure_cookie(user_id)
             self.redirect('/welcome')
         else:
             self.render('signup.html',**params)
 
-<<<<<<< HEAD
 class Login(Handler):
     def get(self):
         user_id = self.check_id_cookie()
@@ -135,83 +139,124 @@ class Logout(Handler):
     def get(self):
         self.response.headers.add_header('Set-Cookie','user_id=;PATH=/')
         self.redirect('/login')
-=======
-
-
-# class Login(Handler):
-#     def get(self):
-        
-
-
->>>>>>> parent of e6aefb3... Standardized all pages using templates and macros
 
 class Welcome(Handler):
     def get(self):
         u = self.check_id_cookie()
         if u:
-            self.render('welcome.html',user = u.username)
+            posts = db.GqlQuery("select * from PostObject where posted_by = '%s' order by created desc"%u.username).fetch(limit=None)
+            self.render('welcome.html',user = u,posts = posts)
         else:
-            self.redirect('/signup')
+            self.redirect('/login')
 
-<<<<<<< HEAD
 class PostsBy(Handler):
     def get(self,poster):
         user = self.check_id_cookie()
-        logging.info("posts by method ran")
         posts = db.GqlQuery("select * from PostObject where posted_by = '%s' order by created desc"%poster).fetch(limit=None)
         self.render('postsby.html',posts = posts,user = user,poster = poster)
    
-=======
-
-
-    
->>>>>>> parent of e6aefb3... Standardized all pages using templates and macros
 class NewPost(Handler):
     def get(self):
-        self.render('newpost.html')
+        u = self.check_id_cookie()
+        if u:
+            self.render('newpost.html',user = u)
+        else:
+            self.redirect('/login')
 
     def post(self):
+        u = self.check_id_cookie();
+        if not u:
+            redirect('/login')
         title = self.request.get('subject')
+        summary = self.request.get('summary')
         code = self.request.get('content')
-        if title and code:
-            new_entery = codeobject(title = title, code = code)
+        
+        if title and code and summary:
+            new_entery = PostObject(title = title,summary = summary,code = code,posted_by = u.username)
             key = new_entery.put()
             entry_id = '/' + str(key.id())
             logging.info(entry_id)
             self.redirect(entry_id)
         else:
-            logging.info('Error with post')
-            self.render('newpost.html',error_message = 'Both a title and code is required!')
+            self.render('newpost.html',error_message = 'All fields are required!',title = title,summary = summary,code = code,user = u)
+
+class EditPost(Handler):
+    def get(self,post_id):
+        u = self.check_id_cookie()
+        if not u:
+            redirect('/login')
+        e = PostObject.get_by_id(int(post_id))
+        self.render('newpost.html',title = e.title,summary = e.summary, code = e.code, user = u)
+
+    def post(self,post_id):
+        logging.info('post id is: '+post_id)
+        u = self.check_id_cookie()
+        if not u:
+            redirect('/login')
+        e = PostObject.get_by_id(int(post_id))
+        title = self.request.get('subject')
+        summary = self.request.get('summary')
+        code = self.request.get('content')
+
+        if title and code and summary:
+            e.title = title
+            e.summary = summary
+            e.code = code
+            e.posted_by = u.username
+            e.put()
+            self.redirect('../'+post_id)
+        else:
+            self.render('newpost.html',error_message = 'All fields are required!',title = title,summary = summary,code = code,user = u)
+
+class DeletePost(Handler):
+    def get(self,post_id):
+        u = self.check_id_cookie()
+        if not u:
+            redirect('/login')
+        e = PostObject.get_by_id(int(post_id))
+        e.delete()
+        time.sleep(0.4)
+        self.redirect('/welcome')
+
+class LikePost(Handler):
+    def get(self,post_id):
+        u = self.check_id_cookie()
+        if not u:
+            self.redirect('/login')
+        e = PostObject.get_by_id(int(post_id))
+        if u.username in e.likes:
+            e.likes.remove(u.username)
+        else:
+            e.likes.append(u.username)
+        e.put()
+        # self.redirect('/')
 
 class Entery(Handler):
     def get(self,post_id):
-        entery = codeobject.get_by_id(int(post_id))
+        u = self.check_id_cookie()
+        entry = PostObject.get_by_id(int(post_id))
 
-        self.render('entery.html',entery = entery)
+        self.render('entery.html',entry = entry,user = u)
 
-<<<<<<< HEAD
 class CommentObject(db.Model):
     comment = db.TextProperty(required = True)
     post_id = db.IntegerProperty(required = True)
     poster_id = db.IntegerProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
-=======
->>>>>>> parent of e6aefb3... Standardized all pages using templates and macros
 
-
-class codeobject(db.Model):
+class PostObject(db.Model):
     title = db.StringProperty(required = True)
+    summary = db.TextProperty(required = True)
     code = db.TextProperty(required = True)
-<<<<<<< HEAD
     posted_by = db.StringProperty(required = True)
-=======
->>>>>>> parent of e6aefb3... Standardized all pages using templates and macros
+    likes = db.StringListProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
 class User(db.Model):
     username = db.StringProperty(required = True)
     password = db.StringProperty(required = True)
     email = db.StringProperty()
+    likes = db.StringListProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
 app = webapp2.WSGIApplication([
@@ -219,7 +264,13 @@ app = webapp2.WSGIApplication([
     ('/newpost',NewPost),
     ('/([0-9]+)',Entery),
     ('/signup',Signup),
-    ('/welcome',Welcome)
+    ('/welcome',Welcome),
+    ('/login',Login),
+    ('/logout',Logout),
+    ('/postsby/([a-zA-Z0-9_-]+)',PostsBy),
+    ('/edit/([0-9]+)',EditPost),
+    ('/delete/([0-9]+)',DeletePost),
+    ('/like/([0-9]+)',LikePost)
 ], debug=True)
 
 
